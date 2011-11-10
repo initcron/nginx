2ca0879b5f3c01b995f6904f1b9699422037739b
Gourav Shah <gs@initcron.org>
2011-11-10 15:45:24 +0000

    adding nginx recipes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,35 @@
 class nginx {
 
-      file{'/usr/local/src/nginx-1.0.9.tar.gz'
+      package{['build-essential']:
+        ensure   => present,
+      }
+
+      package{['libpcre3']:
+        ensure   => present,
+      }
+
+      
+      file{'/usr/local/src/nginx-1.0.9.tar.gz':
         ensure  => present,
 	owner   => root,
 	group   => root,
-	source  => 'file://module/nginx/nginx-1.0.9.tar.gz',
+	source  => 'puppet:///modules/nginx/nginx-1.0.9.tar.gz',
       }
 
+      exec{'extract_nginx_source':
+        path     => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+	command  => "tar -xzf nginx-1.0.9.tar.gz",
+	cwd      => "/usr/local/src", 
+	creates  => "/usr/local/src/nginx-1.0.9",
+	require  => File['/usr/local/src/nginx-1.0.9.tar.gz'],
+      }
 
+      exec{'configure_nginx':
+        path     => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+        command  => "./configure",
+        cwd      => "/usr/local/src/nginx-1.0.9",
+        #creates  => "/usr/local/src/nginx-1.0.9",
+        require  => [Exec['extract_nginx_source'], Package['build-essential'], Package[libpcre3] ],
+      }
 
 }
